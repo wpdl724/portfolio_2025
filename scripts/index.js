3aac57962c9d8eeeee84a267d8e92249f5ba36e5
jay <wpdl724@gmail.com>
2025-07-16 13:02:33 +0900

    중간과정

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+// 전체 수직 스크롤
+const wrap = new Swiper('#wrap',{
+    direction: 'vertical',
+    mousewheel: true,
+})
+// 수직 스와이프 이동함수
+// 수직스와이프변수명.slideTop(이동인덱스값, 지속시간)
+// nav.forEach((obj, idx)=>{
+//     console.log(obj, idx);
+//     obj.addEventListener('click', (e)=>{
+//         e.preventDefault(); //a의 href 기본기능 막기
+//         wrap.slideTo(idx, 1000);
+//     })
+// })
+
+// 디자인 슬라이드
+
+/* bnr */
+/* sns */
+// sns 프로젝트 클릭시 팝업 진행 (클릭한 이미지가 팝업 이미지로 교체)
+
+/* detail */
