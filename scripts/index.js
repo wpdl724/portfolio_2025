eee89f3f6042be9d0817345beff7129dd4c1e57c
jay <wpdl724@gmail.com>
2025-07-18 11:39:47 +0900

    수정1

--- scripts/index.js
+++ scripts/index.js
@@ -13,47 +13,56 @@ const wrap = new Swiper('#wrap',{
 //     })
 // })
 
-// 디자인 슬라이드
-const bnr = new Swiper('#bnr_swiper',{
-    slidesPerView:2,
-    spaceBetween:10,
-    autoplay:{delay:0,},
+
+/* 디자인 프로젝트 */
+const popup_bg = document.querySelector('.popup_bg')
+const bannerProject = document.querySelectorAll('#project2 .banner img')
+const designProject = document.querySelectorAll('#project2 .snsDetail img')
+popup_bg.style.display = 'none'/* 팝업 초반 숨기기 */
+
+for(let i of designProject){/* SNS디자인, 상세디자인 클릭 시 팝업 출력 JS */
+    i.addEventListener('click',function(){
+        popup_bg.style.display = 'flex'
+        popup_bg.children[0].scrollTo(0,0);
+        popup_bg.children[0].style.width = '800px';/* SNS,상세디자인 팝업 출력 크기 */
+        popup_bg.children[0].style.height = '800px';
+        popup_bg.children[0].children[0].src = i.src;
+        wrap.mousewheel.disable();/* 마우스 휠 막기 */
+    })
+}
+for(let i of bannerProject){/* 배너디자인 클릭 시 팝업 출력 JS */
+    i.addEventListener('click',function(){
+        popup_bg.style.display = 'flex'
+        popup_bg.children[0].style.width = '1000px';/* 배너디자인 팝업 출력 크기 */
+        popup_bg.children[0].style.height = 'max-content';
+        popup_bg.children[0].children[0].src = i.src;
+        wrap.mousewheel.disable();/* 마우스 휠 막기 */
+    })
+}
+
+/* 배너디자인 프로젝트 소개 */
+const designslide1 = new Swiper('#design_project2 .design1', {
+    autoplay:{delay:0, desableOnInteraction: false,},
     speed:4000,
     loop:true,
-})
-const sns = new Swiper('#sns_swiper',{
-    slidesPerView:2,
+    slidesPerView:3,
     spaceBetween:10,
-    autoplay:{delay:0,},
-    speed:4000,
-    loop:true,
 })
-const detail = new Swiper('#detail_swiper',{
-    slidesPerView:2,
+/* SNS디자인 프로젝트 소개 */
+const designslide2 = new Swiper('#design_project2 .design2', {
+    autoplay:{delay:0, desableOnInteraction: false, },
+    speed:6000,
+    loop:true,
+    slidesPerView:3,
     spaceBetween:10,
-    autoplay:{delay:0,},
-    speed:4000,
-    loop:true,
 })
-
-// sns 프로젝트 클릭시 팝업 진행 (클릭한 이미지가 팝업 이미지로 교체)
-// const snsProject = document.querySelectorAll('#bnr_swiper .swiper-slide')
-// const popup = document.querySelector('.popup_bg');
-// console.log(snsProject,popup);
-
-// for(let sns of snsProject){
-//     sns.addEventListener('click',()=>{
-//         popup.style.display = 'block';
-//         popup.children[0].children[0].src = sns.children[0].src;
-//         //팝업 실행 시 전체 수직 Swiper 스크롤 기능 막기
-//         wrap.mousewheel.disable(); // 스크롤 기능 풀기=> eanble()
-//     })
-// }
-
-// /* popup_bg */
-// popup.addEventListener('click', ()=>{
-//     popup.style.display='none'
-//     wrap.mousewheel.eanable();
-// })
-
-/* detail */
+/* 상세디자인 프로젝트 소개 */
+const designslide3 = new Swiper('#design_project2 .design3', {
+    autoplay:{delay:2000,},
+    effect:'fade',
+    loop:true,
+    pagination:{
+        el:'#project2 .swiper-pagination',
+        type:'bullets',
+    },
+})
